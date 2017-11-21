5c30e39025d7076f7914215ffacb689cc8bdff2d
eliskurskiy93 <eliskurskiy93@gmail.com>
2017-11-21 13:25:13 -0800

    removed

--- MyVehicleRepairServiceNeeds/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace MyVehicleRepairServiceNeeds.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- MyVehicleRepairServiceNeeds/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace MyVehicleRepairServiceNeeds.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
